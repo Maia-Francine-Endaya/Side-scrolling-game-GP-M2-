e97fbda1b6caee58ddb4ebf66439ed2b4d79bba9
Maia Francine Endaya <maia.endaya@gmail.com>
2022-04-01 13:45:35 +0800

    added_placeholders_and_game_level

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,6 @@
 var config = {
   type: Phaser.AUTO,
-  width: 800,
+  width: 1770,
   height: 600,
   physics: {
     default: 'arcade',
@@ -16,12 +16,15 @@ var config = {
   }
 };
 
+var game = new Phaser.Game(config);
+
 var scoreText;
 var livesCounter;
+var ground;
 
 function preload() {
-  this.load.image('background', './assets/images/Background-Placeholder.PNG')
-  this.load.image('coin', './assets/images/Coin-Placeholder.PNG')
+  this.load.image('background', './assets/images/Background-Placeholder.PNG');
+  this.load.image('coin', './assets/images/Coin-Placeholder.PNG');
   this.load.image('player', './assets/images/Player-Placeholder.PNG');
   this.load.image('hazard', './assets/images/Hazard-Placeholder.PNG');
   this.load.image('platform', './assets/images/Platform-Placeholder.PNG');
@@ -29,6 +32,14 @@ function preload() {
 
 function create() {
   this.add.image(400, 300, 'background');
+
+  ground = this.physics.add.staticGroup();
+
+  ground.create(400, 580, 'platform');
+  ground.create(1000, 580, 'platform');
+  ground.create(1400, 580, 'platform');
+  ground.create(1700, 580, 'platform');
+  ground.create(750, 220, 'platform');
 };
 
 function update() {
