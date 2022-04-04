d6c258c428b9de0303999804664f9c7b198e176c
Maia Francine Endaya <maia.endaya@gmail.com>
2022-04-04 13:24:20 +0800

    added_goal_replaced_coin_placeholder

--- scripts/game.js
+++ scripts/game.js
@@ -23,89 +23,104 @@ var coins;
 var platforms;
 
 var cursors;
+var goal;
 
 var score = 0;
 var scoreText;
 var coinsCollected = 0;
 var collectedText;
 
+var win;
+
 function preload() {
   this.load.image('background', './assets/images/Background-Placeholder.PNG');
   this.load.image('ground', './assets/images/platform.png');
-  this.load.image('coin', './assets/images/Coin-Placeholder.PNG');
+  this.load.image('coin', './assets/images/Coin.PNG');
+  this.load.image('goal', './assets/images/Goal.PNG');
   this.load.spritesheet('dude',
-    './assets/dude.png', { frameWidth: 32, frameHeight: 48 })
+    './assets/images/dude.png', { frameWidth: 32, frameHeight: 48 })
 };
 
 function create() {
-
-  scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
-  collectedText = this.add.text(16, 24, 'Coins Collected: 0', { fontSize: '32px,', fill: '#000' });
   this.add.image(400, 300, 'background');
   this.add.image(1400, 300, 'background');
+
+  //Score
+  scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
+  collectedText = this.add.text(20, 55, 'Coins Collected: 0', { fontSize: '32px,', fill: '#000' });
 
   //Platforms
   platforms = this.physics.add.staticGroup();
 
   platforms.create(400, 598, 'ground');
 
+  platforms.create(100, 598, 'ground');
   platforms.create(900, 598, 'ground');
   platforms.create(1200, 598, 'ground');
   platforms.create(1700, 598, 'ground');
 
+  platforms.create(1000, 430, 'ground');
+  platforms.create(1400, 510, 'ground');
+  platforms.create(600, 510, 'ground');
+  platforms.create(600, 340, 'ground');
+  platforms.create(200, 260, 'ground');
+
 
   //Player
-  //TO DO: ADD PLAYER, FOR SOME REASON IT CAN'T MOVE LEFT AND RIGHT, BUT CAN JUMP
-  player = this.physics.add.sprite(400, 450, 'dude');
+  player = this.physics.add.sprite(100, 450, 'dude');
 
   player.setBounce(0.2);
   player.setCollideWorldBounds(false);
   player.body.setGravityY(290);
-  player.setOrigin(400, 450);
 
   //Player Animation
   this.anims.create({
     key: 'left',
-    frames: this.anims.generateFrameNumbers('guy', { start: 0, end: 3 }),
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
     frameRate: 10,
     repeat: -1
   });
 
   this.anims.create({
     key: 'turn',
-    frames: [{ key: 'guy', frame: 4 }],
+    frames: [{ key: 'dude', frame: 4 }],
   });
 
   this.anims.create({
     key: 'right',
-    frames: this.anims.generateFrameNumbers('guy', { start: 5, end: 8 }),
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
     frameRate: 10,
     repeat: -1
   });
 
-  //Game Camera
-  game.camera.follow(player);
-
   //Coins
+
+  var coinDistance = Phaser.Math.FloatBetween(100, 150);
+
   coins = this.physics.add.group({
     key: 'coin',
-    repeat: 11,
-    setXY: { x: 12, y: 0, stepX: 70 }
+    repeat: 12,
+    setXY: { x: 100, y: 0, stepX: coinDistance }
   });
 
   coins.children.iterate(function (child) {
-    child.setBounceY(Phaser.Math.FloatBetween(0.1, 0.2));
+    child.setBounceY(Phaser.Math.FloatBetween(0.2, 0.4));
   });
+
+  //Goal
+  goal = this.physics.add.staticGroup();
+
+  goal.create(40, 220, 'goal');
 
   //Cursors
   cursors = this.input.keyboard.createCursorKeys();
 
   //Physics
   this.physics.add.collider(player, platforms);
-  this.physics.add.collider(stars, platforms);
+  this.physics.add.collider(coins, platforms);
 
   this.physics.add.overlap(player, coins, collectCoin, null, this);
-
+  this.physics.add.overlap(player, goal, winFunction, null, this);
 };
 
 function update() {
@@ -132,11 +147,19 @@ function update() {
 
 //Function of adding score and number of coins collected
 function collectCoin(player, coins) {
-  coins.disabledBody(true, true);
+
+  coins.disableBody(true, true);
 
   score += 10;
   coinsCollected += 1;
 
   scoreText.setText('Score: ' + score);
   collectedText.setText('Coins Collected: ' + coinsCollected);
+}
+
+//Function of ending the game when goal is reached
+function winFunction(player, goal) {
+  this.physics.pause();
+  player.anims.play('turn');
+  win = true;
 }
