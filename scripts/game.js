76f4dd8d5e60aee6c0389633fa34564d69a26c3b
Maia Francine Endaya <maia.endaya@gmail.com>
2022-04-05 10:15:39 +0800

    added_environmental_hazards

--- scripts/game.js
+++ scripts/game.js
@@ -21,6 +21,7 @@ var game = new Phaser.Game(config);
 var player;
 var coins;
 var platforms;
+var hazard;
 
 var cursors;
 var goal;
@@ -29,15 +30,19 @@ var score = 0;
 var scoreText;
 var coinsCollected = 0;
 var collectedText;
+var lives = 3;
+var livesCounter;
 var winText;
 
 var win;
+var gameOver;
 
 function preload() {
   this.load.image('background', './assets/images/Background-Placeholder.PNG');
   this.load.image('ground', './assets/images/platform.png');
   this.load.image('coin', './assets/images/Coin.PNG');
   this.load.image('goal', './assets/images/Goal.PNG');
+  this.load.image('hazard', './assets/images/Hazard-Placeholder.PNG')
   this.load.spritesheet('dude',
     './assets/images/dude.png', { frameWidth: 32, frameHeight: 48 })
 };
@@ -47,8 +52,11 @@ function create() {
   this.add.image(1400, 300, 'background');
 
   //Score
-  scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
+  scoreText = this.add.text(400, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
   collectedText = this.add.text(20, 55, 'Coins Collected: 0', { fontSize: '32px,', fill: '#000' });
+
+  //Lives
+  livesCounter = this.add.text(16, 16, 'Lives: 3', { fontSize: '32px', fill: '#000' });
 
   //Platforms
   platforms = this.physics.add.staticGroup();
@@ -62,7 +70,7 @@ function create() {
 
   platforms.create(1000, 430, 'ground');
   platforms.create(1400, 510, 'ground');
-  platforms.create(600, 510, 'ground');
+  // platforms.create(600, 510, 'ground');
   platforms.create(600, 340, 'ground');
   platforms.create(200, 260, 'ground');
 
@@ -95,13 +103,10 @@ function create() {
   });
 
   //Coins
-
-  var coinDistance = Phaser.Math.FloatBetween(100, 150);
-
   coins = this.physics.add.group({
     key: 'coin',
-    repeat: 12,
-    setXY: { x: 100, y: 0, stepX: coinDistance }
+    repeat: 10,
+    setXY: { x: 100, y: 0, stepX: 130 }
   });
 
   coins.children.iterate(function (child) {
@@ -110,8 +115,18 @@ function create() {
 
   //Goal
   goal = this.physics.add.staticGroup();
-
   goal.create(40, 220, 'goal');
+
+  //Hazards
+  hazards = this.physics.add.staticGroup();
+
+  hazards.create(300, 558, 'hazard');
+  hazards.create(425, 558, 'hazard');
+  hazards.create(473, 558, 'hazard');
+
+  hazards.create(1630, 558, 'hazard');
+  hazards.create(1680, 558, 'hazard');
+  hazards.create(1730, 558, 'hazard');
 
   //Cursors
   cursors = this.input.keyboard.createCursorKeys();
@@ -122,6 +137,7 @@ function create() {
 
   this.physics.add.overlap(player, coins, collectCoin, null, this);
   this.physics.add.overlap(player, goal, winFunction, null, this);
+  this.physics.add.overlap(player, hazards, loseLives, null, this);
 };
 
 function update() {
@@ -170,4 +186,22 @@ function winFunction(player, goal) {
   player.setTint(0xffd000);
   player.anims.play('turn');
   win = true;
+}
+
+//Function of subtracting lives as player collides with hazards
+function loseLives(player, hazards) {
+
+  lives -= 1;
+  livesCounter.setText('Lives Left: ' + lives);
+
+  //TO DO: Add an invincibility timer so that lives won't get lost very fast
+
+  if (lives == 0) {
+    this.physics.pause();
+    player.setTint(0xbd0000);
+    player.anims.play('turn');
+
+    gameOverText = this.add.text(600, 300, 'YOU LOST!', { fontSize: '100px', fill: '#000' })
+    gameOver = true;
+  }
 }
